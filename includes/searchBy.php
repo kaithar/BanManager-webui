b0cab2c42cb6e0eac2946e94acd8670e87a99c84
Daniel Bradshaw <Contact.on.Github>
2015-01-05 02:53:33 +0000

    Add missing copyright.

--- includes/searchBy.php
+++ includes/searchBy.php
@@ -1,4 +1,13 @@
 <?php
+/*  BanManagement © 2012, a web interface for the Bukkit plugin BanManager
+    by James Mortemore of http://www.frostcast.net
+    is licenced under a Creative Commons
+    Attribution-NonCommercial-ShareAlike 2.0 UK: England & Wales.
+    Permissions beyond the scope of this licence 
+    may be available at http://creativecommons.org/licenses/by-nc-sa/2.0/uk/.
+    Additional licence terms at https://raw.github.com/confuser/Ban-Management/master/banmanagement/licence.txt
+*/
+
 if (!defined('INTERNAL'))
     die("Don't call me directly!");
 
